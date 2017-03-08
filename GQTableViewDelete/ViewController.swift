af0be620f23b49e2d543cce80b5dcba1a6690d16
apple <apple@appledemacbook-air.local>
2017-03-08 18:55:44 +0800

    Initial Commit

---
+++ GQTableViewDelete/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GQTableViewDelete
+//
+//  Created by apple on 17/3/8.
+//  Copyright © 2017年 GQ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
